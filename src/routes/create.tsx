6825cee1bc830e749ab1adbafdb43e94e6fc4c00
blaugrana11 <21168@ecam.be>
2025-04-13 21:06:35 +0200

    redirect to login if not logged for add listing form

--- src/routes/create.tsx
+++ src/routes/create.tsx
@@ -1,11 +1,22 @@
 // /routes/create.tsx
 import AddListingForm from "~/components/AddListingForm";
+import { getUser } from "~/lib/auth/user";
+import { redirect } from "@solidjs/router";
+
+export const route = {
+  preload: async () => {
+    const user = await getUser();
+    if (!user) {
+      throw redirect("/login");
+    }
+  },
+};
 
 export default function CreatePage() {
   return (
     <div class="min-h-screen flex items-start justify-center bg-white-50 py-10">
       <div class="w-full max-w-2xl">
-        <h1 class="text-3xl font-semibold text-center text-[#600AFF] mb-8  mt-6">
+        <h1 class="text-3xl font-semibold text-center text-[#600AFF] mb-8 mt-6">
           Place an Ad
         </h1>
         <AddListingForm />
@@ -13,9 +24,3 @@ export default function CreatePage() {
     </div>
   );
 }
-
-
-
-
-
-
